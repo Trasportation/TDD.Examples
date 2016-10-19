dc26121ea32291744c4d043e3ffc0d065ff85df8
Roberto Beccari <info@robertobeccari.it>
2016-10-19 14:48:54 +0200

    New examples

---
+++ StringCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorUnitTest
+    {
+        StringCalculator sc = new StringCalculator();
+
+        [TestMethod]
+        public void Add_SingleNumber_Return1()
+        {
+            sc.Add("1");
+            Assert.AreEqual(sc.Result, 1);
+        }
+
+        [TestMethod]
+        public void Add_SingleNumber_ReturnSumResult()
+        {
+            sc.Add("1");
+            sc.Add("1");
+            Assert.AreEqual(sc.Result, 2);
+        }
+
+        [TestMethod]
+        public void Add_MultipleNumbers_ReturnSumResult()
+        {
+            sc.Add("1,2");
+            Assert.AreEqual(sc.Result, 3);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        private int result = 0;
+
+        public int Result {
+            get {
+                return result;
+            }
+        }
+
+        // http://osherove.com/tdd-kata-1/
+
+        public void Add(string numbers)
+        {
+            result += int.Parse(numbers);
+        }
+    }
+}
